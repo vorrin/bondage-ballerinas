bbadefd1346370bd164bdcf95a148818aef1daff
Maurizio <vorrin.tal@gmail.com>
2014-06-07 18:55:07 +0100

    FUCKING FUCKER FUCK fixed ship.cs

---
+++ Assets/Scripts/Bonus.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bonus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter( Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            //Player should be awarded stuff
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
