65a9da1977406a3576c96a41fba9db08d01de810
Maurizio <vorrin.tal@gmail.com>
2014-06-07 15:18:01 +0100

    messing with bond

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -7,18 +7,24 @@ public class Ship : MonoBehaviour {
     public float engineStrength = 10f;
     public KeyCode thrustButton;
     public KeyCode latchButton;
-    private bool latched = false;
-    public bool tryingToLatch = false;
-    public GameObject ropePrefab;
-    public Dictionary<Ship, UltimateRope> connections = new Dictionary<Ship, UltimateRope>();
 
+    public GameObject ropePrefab;
+    public Dictionary<Ship, Dictionary<ButtonColors, UltimateRope>> connections = new Dictionary<Ship, Dictionary<ButtonColors, UltimateRope>>();
+    public ButtonColors debugShipColor;
+    public float maximumDistanceForLatching = 20f;
     public enum ButtonColors
     {
         Red,
         Green,
         Blue,
-        Yellow
+        Yellow,
+        Null
     }
+
+
+    private ButtonColors tryingToLatch = ButtonColors.Null;
+    private bool latched = false;
+
 
 	// Use this for initialization
 	void Start () {
@@ -29,30 +35,41 @@ public class Ship : MonoBehaviour {
         rigidbody.AddForce( transform.up * (engineStrength * deltaTime ) );
     }
 
-    public void TryLatching() //color option to go in here?
+    public void TryLatching(ButtonColors currentColor) //color option to go in here?
     {
-        tryingToLatch = true;
+        tryingToLatch = currentColor;
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
         foreach (GameObject player in players)
         {
             Ship currentShip = player.GetComponent<Ship>();
-            if (currentShip.tryingToLatch && currentShip != this)
+            if (currentShip.tryingToLatch != ButtonColors.Null && currentShip.tryingToLatch == currentColor && currentShip != this)
             {
                 if (connections.ContainsKey(currentShip))
                 {
+                    //Already bound, nothing done.
                     return;
                 }
                 else
                 {
-                    
+                    float currentDistance = Vector3.Distance(gameObject.transform.position, currentShip.transform.position);
+                    if (currentDistance > maximumDistanceForLatching)
+                    {
+                        print("too far");
+                        continue;
+                        //too far, nothing done.
+                    }
                     GameObject rope = Instantiate(ropePrefab as Object) as GameObject;
                     UltimateRope ropeComponent = rope.GetComponent<UltimateRope>();
-                    
-                    connections.Add(currentShip, ropeComponent);
-                    currentShip.connections.Add(this, ropeComponent);
+
+                    //Dictionary<ButtonColors, UltimateRope> tmpDict = new Dictionary<ButtonColors, UltimateRope>() {
+                    //    { currentColor,ropeComponent}
+                    //};
+
+                    connections.Add(currentShip, new Dictionary<ButtonColors, UltimateRope>() {  { currentColor, ropeComponent} });
+                    currentShip.connections.Add(this, new Dictionary<ButtonColors, UltimateRope>() { { currentColor, ropeComponent } });
 
                     ropeComponent.RopeStart = transform.FindChild("Hook").gameObject;
-                    ropeComponent.RopeNodes[0].fLength = Vector3.Distance(gameObject.transform.position, currentShip.transform.position);
+                    ropeComponent.RopeNodes[0].fLength = currentDistance; 
                     ropeComponent.RopeNodes[0].goNode = currentShip.transform.FindChild("Hook").gameObject;
                     ropeComponent.Regenerate();
 
@@ -71,9 +88,35 @@ public class Ship : MonoBehaviour {
         }
         if (Input.GetKey(latchButton))
         {
-            TryLatching();
+            if (tryingToLatch != ButtonColors.Null  )
+            {
+                return;
+            }
+            TryLatching( debugShipColor );
+        }
+        if (Input.GetKeyUp(latchButton))
+        {
+            Unlatch(debugShipColor);
+
         }
 
 	
 	}
+
+    void Unlatch(ButtonColors unlatchingColor)
+    {
+        tryingToLatch = ButtonColors.Null;
+        foreach (Ship ship in connections.Keys)
+        {
+            Dictionary<ButtonColors, UltimateRope> connection = connections[ship];
+            if (connection.ContainsKey(unlatchingColor))
+            {
+                Destroy(connection[unlatchingColor].gameObject);
+                ship.connections.Remove(this);
+            }
+            connections.Remove(ship);
+        }
+
+
+    }
 }
