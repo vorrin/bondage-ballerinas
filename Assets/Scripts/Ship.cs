5b4004e3ad945cbd5693f78eb4f09b881896f25e
Maurizio <vorrin.tal@gmail.com>
2014-06-07 12:24:19 +0100

    base scene

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+
+    public float engineStrength = 10f;
+    public KeyCode thrustButton;
+    public KeyCode latchButton;
+    private bool latched = false; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void EngineOn(float deltaTime){
+        rigidbody.AddForce( transform.up * (engineStrength * deltaTime ) );
+    }
+
+    public void TryLatching()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(thrustButton))
+        {
+            EngineOn(Time.deltaTime);
+        }
+        if (Input.GetKey(latchButton))
+        {
+            TryLatching();
+        }
+
+	
+	}
+}
