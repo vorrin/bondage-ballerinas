0272bf54c9d449765012e7bed3f4d087810cc610
Petri Autio <archbang85@gmail.com>
2014-06-07 19:01:18 +0100

    Trails and walls

--- Assets/Scripts/BasicMove.cs
+++ Assets/Scripts/BasicMove.cs
@@ -11,14 +11,19 @@ public class BasicMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		int yMovement = 0;
+		int xMovement = 0;
 
 		if (Input.GetKey ("w")) {
 			yMovement = 1;
 
 		} else if (Input.GetKey ("s")) {
 			yMovement = -1;
+		} else if (Input.GetKey ("d")) {
+			xMovement = 1;	
+		} else if (Input.GetKey ("a")) {
+			xMovement = -1;
 		}
 
-		this.transform.rigidbody.AddForce(new Vector3(0, yMovement, 0));
+		this.transform.rigidbody.AddForce(new Vector3(xMovement, yMovement, 0));
 	}
 }
