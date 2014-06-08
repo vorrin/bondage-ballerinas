c672bfa12721d2ecf98d86ec79cac8f1dabe061c
Maurizio <vorrin.tal@gmail.com>
2014-06-08 12:57:47 +0100

    bondage ballerinas, the game it should have been -_-

---
+++ Assets/Scripts/DebuggerScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebuggerScript : MonoBehaviour {
+    Ship ship;
+    public Ship.ButtonColors buttonColor;
+	// Use this for initialization
+	void Start () {
+        ship = GetComponent<Ship>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ship.TryLatching(buttonColor);
+	}
+}
